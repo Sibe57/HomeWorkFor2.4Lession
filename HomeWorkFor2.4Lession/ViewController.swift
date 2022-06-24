e329a760ee62ad2f95c9b5810611bba0e58e7e0d
Fedor Eronin <eroninf@icloud.com>
2022-06-24 13:40:19 +0300

    add main logic

--- HomeWorkFor2.4Lession/ViewController.swift
+++ HomeWorkFor2.4Lession/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    
+    //MARK: - Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - IBActions
+    @IBAction func sligersChanged() {
+        
+        let color = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+        colorView.backgroundColor = color
+        
+        let roundedRedValue = (redSlider.value * 100).rounded() / 100
+        let roundedGreenValue = (greenSlider.value * 100).rounded() / 100
+        let roundedBlueValue = (blueSlider.value * 100).rounded() / 100
+        redValue.text = String(roundedRedValue)
+        greenValue.text = String(roundedGreenValue)
+        blueValue.text = String(roundedBlueValue)
+        
+    }
 }
 
