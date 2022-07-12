51e4caebe09751969e8f1c022a4861676a5c50d7
Fedor Eronin <eroninf@icloud.com>
2022-07-12 21:17:22 +0300

    add main ViewController

---
+++ HomeWorkFor2.4Lession/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  HomeWorkFor2.4Lession
+//
+//  Created by Федор Еронин on 12.07.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
