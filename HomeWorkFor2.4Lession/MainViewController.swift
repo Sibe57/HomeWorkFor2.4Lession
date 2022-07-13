eb5369ef95df66beb575e9a70d326e26f21de266
Fedor Eronin <eroninf@icloud.com>
2022-07-13 13:21:14 +0300

    create prepareForSegue method

--- HomeWorkFor2.4Lession/MainViewController.swift
+++ HomeWorkFor2.4Lession/MainViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsViewController = segue.destination as? SettingsViewController
+        else {
+            return
+        }
+        settingsViewController.colorMainView = view.backgroundColor
     }
 }
